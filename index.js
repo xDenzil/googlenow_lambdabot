2565ef8590be56c50a3a7f9bcad79ca35b25aa9c
Denzil Williams <denzilwilliams@Denzils-MacBook-Pro.local>
2020-04-05 17:11:29 -0500

    echoText ot userINput

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ restService.post("/echo", function (req, res) {
   var speech =
     req.body.queryResult &&
     req.body.queryResult.parameters &&
-    req.body.queryResult.parameters.echoText
-      ? req.body.queryResult.parameters.echoText
+    req.body.queryResult.parameters.userInput
+      ? req.body.queryResult.parameters.userInput
       : "Shalleluyaj";
 
   var speechResponse = {
