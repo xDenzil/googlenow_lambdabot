e4159bccdce319cf4b4448ba126e3eacd815f70d
Denzil Williams <denzzilwilliams@gmail.com>
2020-04-06 18:15:28 -0500

    add more dependencies

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const http = require("http");
 
-
 const hostname = "127.0.0.1";
 const port = 3000;
 
@@ -11,6 +10,13 @@ const server = http.createServer((req, res) => {
   res.end("Hello");
   //console.log(dict["Close Bracket"]);
 });
+
+LPAREN: "(";
+RPAREN: ")";
+LAMBDA: "λ"; /* we'll also allow `\` for convenience */
+DOT: ".";
+// lowercase identifier: lowercase letter followed by any letters
+//LCID: /[a-z][a-zA-Z]*/
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
--- index.js
+++ index.js
@@ -43,7 +43,6 @@ function parseLambda(input) {
       }
     }
   }
-
   return processedString;
 }
 
