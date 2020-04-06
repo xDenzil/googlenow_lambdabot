23d5d6328eccaca9011cc04cfcc918dfa41c58ed
Denzil Williams <denzilwilliams@Denzils-MacBook-Pro.local>
2020-04-05 19:32:38 -0500

    speech parse try

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const http = require("http");
 
+
 const hostname = "127.0.0.1";
 const port = 3000;
 
@@ -10,50 +11,6 @@ const server = http.createServer((req, res) => {
   res.end("Hello");
   //console.log(dict["Close Bracket"]);
 });
-
-const inputString =
-  "Open bracket lambda x dot x close bracket Space open Bracket lambda y dot y close bracket close bracket";
-
-var dict = {
-  open: "(",
-  close: ")",
-  lambda: "λ",
-  x: "x",
-  y: "y",
-  z: "z",
-  a: "a",
-  b: "b",
-  c: "c",
-  space: " ",
-  dot: ".",
-};
-
-var accepted = [
-  "open",
-  "close",
-  "lambda",
-  "x",
-  "y",
-  "z",
-  "a",
-  "b",
-  "c",
-  "space",
-  "dot",
-];
-
-var processedString = "";
-var split = inputString.split(" ");
-
-for (i = 0; i < split.length; i++) {
-  for (j = 0; j < accepted.length; j++) {
-    if (split[i].toLowerCase() == accepted[j]) {
-      processedString += dict[split[i].toLowerCase()];
-    }
-  }
-}
-
-console.log("String:" + processedString);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
--- index.js
+++ index.js
@@ -1,5 +1,49 @@
 "use strict";
 
+var dict = {
+  open: "(",
+  close: ")",
+  lambda: "λ",
+  x: "x",
+  y: "y",
+  z: "z",
+  a: "a",
+  b: "b",
+  c: "c",
+  space: " ",
+  dot: ".",
+};
+
+var accepted = [
+  "open",
+  "close",
+  "lambda",
+  "x",
+  "y",
+  "z",
+  "a",
+  "b",
+  "c",
+  "space",
+  "dot",
+];
+
+function parseLambda(input) {
+  const inputString = input;
+  var processedString = "";
+  var split = input.split(" ");
+
+  for (i = 0; i < split.length; i++) {
+    for (j = 0; j < accepted.length; j++) {
+      if (split[i].toLowerCase() == accepted[j]) {
+        processedString += dict[split[i].toLowerCase()];
+      }
+    }
+  }
+
+  return processedString;
+}
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -28,7 +72,7 @@ restService.post("/echo", function (req, res) {
         items: [
           {
             simpleResponse: {
-              textToSpeech: "Oh",
+              textToSpeech: parseLambda(speech),
             },
           },
         ],
